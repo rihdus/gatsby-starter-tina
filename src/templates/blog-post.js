3ccf72fb0933d4633d64b5467c7d6f1137dacc01
Sudhir Meher <sudhir.meher@singtel.com>
2021-03-30 16:06:04 +0800

    cleanup

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,7 +5,7 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { TinaProvider, useCMS, useForm, usePlugin } from 'tinacms';
+import { useCMS, useForm, usePlugin } from 'tinacms';
 
 const BlogPostTemplate = ({ data, location }) => {
   const cms = useCMS();
